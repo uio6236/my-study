891a20f9922be428a8cb5d5f742c1c3474b19b36
uio6236 <uio6236@naver.com>
2026-06-16 11:28:32 +0900

    Practice4 modify

--- Practice4/src/com/kh/ex1/Quiz1.java
+++ Practice4/src/com/kh/ex1/Quiz1.java
@@ -7,7 +7,26 @@ public class Quiz1 {
 		System.out.println("1. 입력\n2. 수정\n3. 조회\n4. 삭제\n7. 종료");
 		System.out.print("메뉴 번호를 입력하세요 : ");
 		int num = sc.nextInt();
-		switch (num) {
+		method2(num);
+		sc.close();
+	}
+	public static void method1(int n) {
+		if (n == 1) {
+			System.out.print("입력 메뉴입니다.");
+		} else if (n == 2){
+			System.out.print("수정 메뉴입니다.");
+		} else if (n == 3){
+			System.out.print("조회 메뉴입니다.");
+		} else if (n == 4){
+			System.out.print("삭제 메뉴입니다.");
+		} else if (n == 7){
+			System.out.print("프로그램을 종료합니다.");
+		} else {
+			System.out.print("잘못된 메뉴 번호입니다.");
+		}
+	}
+	public static void method2(int n) {
+		switch (n) {
 		case 1:
 			System.out.print("입력 메뉴입니다.");
 			break;
@@ -27,7 +46,5 @@ public class Quiz1 {
 			System.out.print("잘못된 메뉴 번호입니다.");
 			break;
 		}
-		sc.close();
 	}
-
 }
